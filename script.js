d0e9d3fba86843f46e9a41edf53916460949ca55
Jasper Pangadlo <41765388+Jasperpangadlo@users.noreply.github.com>
2026-05-21 16:09:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -698,7 +698,7 @@ function updateClock() {
           previewImg.src = "images/bella.png";
         } else if (productName.includes("alessandra")) {
           previewImg.src = "images/alessandra.png";
-          else if (productName.includes("bella (kids)")) {
+        }  else if (productName.includes("bella (kids)")) {
           previewImg.src = "images/bella kids.png";
         } else {
           previewImg.src = "logo.png";
